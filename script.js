392ba9709582c82371220ab884eeb63916804cf3
LGgameLAB <45958481+LGgameLAB@users.noreply.github.com>
2021-12-25 09:31:46 +0000

    Fixed JSON and have basic api and json usage working

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var elem = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elem);
 
-$.getJSON("test.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+var setup = {};
+console.log("IS THIS OPERATIONAL");
 
-var setup= {id:'graph1', latex:'y=x^2'}
-calculator.setExpression(setup);
+function addExpression(value, index, array) {
+        calculator.setExpression(value);
+    };
+
+$.getJSON("test.json", function(json){
+    setup = json;
+    console.log(setup);
+    setup.graph.forEach(addExpression);
+});
